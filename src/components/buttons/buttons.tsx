10bcbfb73f17696a870331c1037eb21c05dda6b1
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-08-09 08:53:08 -0400

    Use string instead of object

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -4,11 +4,8 @@ import { default as debugClickHandler } from "../../debug";
 
 export default function Buttons() {
   return (
-    <div style={{ display: "inline" }}>
-      <button
-        class="btn-topic-normal"
-        onclick={calculateNextPostClickHandler}
-      >
+    <div style="display: inline">
+      <button class="btn-topic-normal" onclick={calculateNextPostClickHandler}>
         Calculate Next Post
       </button>
       <button class="btn-topic-normal inverse" onclick={debugClickHandler}>
