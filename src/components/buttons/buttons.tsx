fcec8965119cf6847ffee3a2739cb0a13469006a
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-08-09 08:41:37 -0400

    Fix lint issues

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -4,8 +4,11 @@ import { default as debugClickHandler } from "../../debug";
 
 export default function Buttons() {
   return (
-    <div style={{display: "inline"}}>
-      <button className="btn-topic-normal" onclick={calculateNextPostClickHandler}>
+    <div style={{ display: "inline" }}>
+      <button
+        className="btn-topic-normal"
+        onclick={calculateNextPostClickHandler}
+      >
         Calculate Next Post
       </button>
       <button className="btn-topic-normal inverse" onclick={debugClickHandler}>
